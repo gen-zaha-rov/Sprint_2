6eb828ae74136cb70789b691b7085c31d427415c
Gennady Zakharov <gen.zaha.rov@yandex.ru>
2025-06-03 15:03:52 +0300

    Выполнить задание 4_ver_2

--- task_4.py
+++ task_4.py
@@ -7,22 +7,24 @@ class EmployeeSalary:
         self.rest_days = rest_days
         self.email = email
 
-    def get_hours(self):
-        if self.hours is None:
-            if self.rest_days is None:
-                return 0
-            return (7 - self.rest_days) * 8 
-        return self.hours
+    @classmethod
+    def get_hours(cls, name, hours=None, rest_days=None, email=None):
+        if hours is None:
+            if rest_days is None:
+                hours = 0
+            else:
+                hours = (7 - rest_days) * 8 
+        return cls(name, hours, rest_days, email)
 
-    def get_email(self):
-        if self.email is None:
-            return f"{self.name.lower().replace(' ', '.')}@email.com"
-        return self.email
+    @classmethod
+    def get_email(cls, name, hours=None, rest_days=None, email=None):
+        if email is None:
+            email =  f"{name.lower().replace(' ', '.')}@email.com"
+        return cls(name, hours, rest_days, email)
     
     @classmethod
     def set_hourly_payment(cls, new_rate):
         cls.hourly_payment = new_rate
   
     def salary(self):
-        fact_hours = self.get_hours()
-        return fact_hours * self.hourly_payment 
+        return self.hours * self.hourly_payment
